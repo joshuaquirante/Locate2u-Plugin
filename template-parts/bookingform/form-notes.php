fabc71b17882a4af6a89f0fcaed68e4f6791ae39
joshuaquirante <105628616+joshuaquirante@users.noreply.github.com>
2022-06-09 16:28:36 +0800

    Delete template-parts directory

--- template-parts/bookingform/form-notes.php
+++
@@ -1,6 +0,0 @@
-<div class="form-section b2 mt-5">
-	<h2 class="text-center form-section-title mb-4">Notes</h2>
-	<div class="input-wrapper">
-		<textarea class="note-textbox w-100" maxlength="200" name="note" placeholder="Additional notes"></textarea>
-	</div>
-</div>
